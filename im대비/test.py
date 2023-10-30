c6145e801db15769304081c8d13bde453db813fd
tmdwoghd <tmdwoghd@naver.com>
2023-10-30 17:58:59 +0900

    1030

--- im대비/test.py
+++ im대비/test.py
@@ -1,30 +1,33 @@
-# 색종이(10163)
+C,R = map(int,input().split())
+K = int(input())
 
-# 보드에 N개의 색종이를 놓았을 때
-# 각 색종이의 넓이를 구해라!
-# 겹쳐지는 부분이 있다면 그 넓이를 빼야함.
+# 남동북서
+# dr = [1,0,-1,0]
+# dc = [0,1,0,-1]
 
-import sys
-sys.stdin = open('input.txt','r')
-
-# 보드판
-arr = [[0]*1001 for _ in range(1001)]
-
-max_r = max_c = 0 
-N = int(input()) # 색종이 장수
-for n in range(1,N+1):
-    sj,si,w,h = map(int, sys.stdin.readline().split()) # 가장 왼쪽 아래 칸의 좌표와 너비,높이
-    max_r = (si+h) if max_r < (si+h) else max_r
-    max_c = (sj+w) if max_c < (sj+w) else max_c
-    for i in range(si,si+h):
-        arr[i][sj:sj+w] = [n]*w
+# 1로 둘러싸인 콘서트장 만들기 (범위 체크 필요없음)
+# concert = [[1] * (C+2)] + [ [1]+[0]*C+[1] for _ in range(R)] + [[1] * (C+2)]
 
 
-cnt = [0]*(N+1)
-for i in range(max_r):
-    for j in range(max_c):
-        cnt[arr[i][j]] += 1
+# 자리 배정이 불가능한 경우 : 0
+if R*C < K:
+    print(0)
 
-# cnt.pop(0) 
-for answer in cnt[1:N+1]:
-    print(answer)
+# 자리 배정하면서 K가 되면 그때 좌표 출력
+else:
+
+    di, dj = [1,0,-1,0], [0,1,0,-1]
+    arr = [[1] * (C+2)] + [[1]+[0]*C+[1] for _ in range(R)] + [[1] * (C+2)]
+
+    # 시작값, 방향
+    ci,cj,d = 1,1,0
+    for n in range(1,K):
+        arr[ci][cj] = n
+        ni = ci + di[d]
+        nj = cj + dj[d]
+        if arr[ni][nj] == 0:
+            ci,cj = ni,nj
+        else:
+            d = (d + 1) % 4
+            ci,cj = ci+di[d] , cj +dj[d]
+    print(f'{cj} {ci}')
