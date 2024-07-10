4328402a001e0d670b9b27caa15a7d965401d42b
tmdwoghd <tmdwoghd@naver.com>
2024-07-10 17:26:50 +0900

    0710

--- im/prtc.py
+++ im/prtc.py
@@ -2,27 +2,10 @@ import sys
 sys.stdin = open('input.txt','r')
 input = sys.stdin.readline
 
+answer = set()
+for _ in range(10):
+    x = int(input())
+    y = x % 42
+    answer.add(y)
 
-x = input()
-answer = 0
-for i in x:
-    if i in 'ABC':
-        answer += 2
-    elif i in 'DEF':
-        answer += 3
-    elif i in 'GHI':
-        answer += 4
-    elif i in 'JKL':
-        answer += 5
-    elif i in 'MINO':
-        answer += 6
-    elif i in 'PQRS':
-        answer += 7
-    elif i in 'TUV':
-        answer += 8
-    elif i in 'WXYZ':
-        answer += 9
-    answer += 1
-
-print(answer)
-        
+print(len(answer))
