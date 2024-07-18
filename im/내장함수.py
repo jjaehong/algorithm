5d6586438529a4d5c8d8cb12cec6fd833516582f
tmdwoghd <tmdwoghd@naver.com>
2024-07-18 15:07:15 +0900

    0718

--- im/내장함수.py
+++ im/내장함수.py
@@ -4,7 +4,7 @@ sys.stdin = open('input.txt','r')
 
 # 내장함수 정리
 
-# 리스트,문자열.count('값',start,end) -> 값의 갯수
+# 리스트,문자열.count('값',start,end) -> 값의 갯수, 오오오오 ㄱ래용?
 # 리스트,문자열.index('값') -> 값의 위치(idx)
 
 alphabet = input().upper()
